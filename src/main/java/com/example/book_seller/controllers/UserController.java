e6db968cdb24ac300bd0d3b65b7086020e0c86f5
Viet Anh <dvahust123@gmail.com>
2021-02-27 23:19:20 +0700

    no message

--- src/main/java/com/example/book_seller/controllers/UserController.java
+++ src/main/java/com/example/book_seller/controllers/UserController.java
@@ -1,20 +1,37 @@
 package com.example.book_seller.controllers;
 
+import com.example.book_seller.configs.JwtTokenUtil;
+import com.example.book_seller.models.JwtRequest;
+import com.example.book_seller.models.JwtResponse;
 import com.example.book_seller.models.ResponseMessage;
 import com.example.book_seller.models.entities.User;
+import com.example.book_seller.services.JwtUserDetailsService;
 import com.example.book_seller.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/api/users")
 public class UserController {
 
-    final UserService userService;
+    @Autowired
+    private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
 
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @ResponseBody
@@ -22,12 +39,36 @@ public class UserController {
         return ResponseEntity.ok(userService.register(user));
     }
 
+//    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
+//    @ResponseBody
+//    public ResponseEntity<ResponseMessage<String>> login(@RequestBody User user) {
+//        return ResponseEntity.ok(userService.login(user));
+//    }
+
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
     @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<ResponseMessage<String>> login(@RequestBody User user) {
-        return ResponseEntity.ok(userService.login(user));
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody User user) throws Exception {
+
+        authenticate(user.getEmail(), user.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername("javainuse");
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
     }
-//
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+
 //    @PostMapping(path = "/logout", consumes = "application/json", produces = "application/json")
 //    @ResponseBody
 //    public ResponseEntity<ResponseMessage<String>> logout(@RequestBody User user) {
